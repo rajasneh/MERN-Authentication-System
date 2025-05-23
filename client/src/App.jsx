f00e0705ed8126de262b874479d2ca3c2f1026a5
Asneh Raj <rajasneh91@gmail.com>
2025-05-23 18:02:11 +0530

    Login , logout functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,24 @@
-import React from 'react'
-import { Routes,Route } from 'react-router-dom'
-import Home from './pages/Home'
-import Login from './pages/Login'
-import EmailVerify from './pages/EmailVerify'
-import ResetPass from './pages/ResetPass'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import EmailVerify from "./pages/EmailVerify";
+import ResetPass from "./pages/ResetPass";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
   return (
     <div>
-      <Routes>
-        <Route path='/' element={<Home/>}></Route>
-        <Route path='/login' element={<Login/>}></Route>
-        <Route path='/email-verify' element={<EmailVerify/>}></Route>
-        <Route path='/reset-password' element={<ResetPass/>}></Route>
-
-      </Routes>
+      <ToastContainer/>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/email-verify" element={<EmailVerify />}></Route>
+          <Route path="/reset-password" element={<ResetPass />}></Route>
+        </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
