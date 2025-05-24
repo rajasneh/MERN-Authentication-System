70e0706eb34d7b0be7a1489dc8617412f831040f
Asneh Raj <rajasneh91@gmail.com>
2025-05-24 20:44:30 +0530

    Ready for deployment

--- server/server.js
+++ server/server.js
@@ -15,11 +15,22 @@ const allowedOrigins = ["http://localhost:5173","https://mern-authentication-sys
 
 app.use(express.json());
 app.use(cookieParser());
+// app.use(cors({
+//   origin: allowedOrigins,
+//   credentials: true,
+// }));
 app.use(cors({
-  origin: allowedOrigins,
+  origin: function(origin, callback){
+    // allow requests with no origin like Postman or curl
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      const msg = `The CORS policy for this site does not allow access from the specified Origin.`;
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
   credentials: true,
 }));
-
 //API Endpoints
 app.get("/", (req, res) => {
   res.send("API Working");
