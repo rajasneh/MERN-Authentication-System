0c64e6c34620de9b9133c307ce9bb62ab0d50a57
Asneh Raj <rajasneh91@gmail.com>
2025-05-24 21:10:18 +0530

    Ready for deployment

--- server/middleware/userAuth.js
+++ server/middleware/userAuth.js
@@ -1,23 +1,27 @@
-import  jwt  from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 
-const userAuth = async (req,res,next) => {
-    const {token}=req.cookies;
-    if(!token){
-        return res.json({success:false,message:"Not Authorized, Login again"});
-    }
-    try {
-        const tokenDecode=jwt.verify(token,process.env.JWT_SECRET);
-        if(tokenDecode.id){
-            req.body = req.body || {};
-            req.body.userId=tokenDecode.id;
-        }else{
-            return res.json({success:false,message:"Not Authorized, Login again"})
-        }
+const userAuth = async (req, res, next) => {
+  const { token } = req.cookies;
+  if (!token) {
+    return res.json({ success: false, message: "Not Authorized, Login again" });
+  }
 
-        next();
-    } catch (error) {
-        res.json({success:false,message:error.message});
+  try {
+    const tokenDecode = jwt.verify(token, process.env.JWT_SECRET);
+    if (tokenDecode.id) {
+      req.body = req.body || {};
+      req.body.userId = tokenDecode.id;
+    } else {
+      return res.json({
+        success: false,
+        message: "Not Authorized, Login again",
+      });
     }
+
+    next();
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
 };
 
 export default userAuth;
